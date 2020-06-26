e48801197530e7f1d2636a73d7d12733131042c3
DESKTOP-OVDRLC0\Nezio <nemanja1994@gmail.com>
2020-06-26 12:52:09 +0200

    Initial C
    Started version control on this project (finally).

---
+++ NNotes/NNotes/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NNotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    // make another app to start this exe only if Text.txt is not empty
+    // set that app to run on starup
+
+    public partial class MainWindow : Window
+    {
+        static string localPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        //static string localNNotesPath = localPath + "\\NNotes";
+        static string localNNotesPath = localPath + "\\NNotes\\Test";
+        string textFilePath = localNNotesPath + "\\Text.txt";
+        string settingsFilePath = localNNotesPath + "\\Settings.txt";
+        string darkTheme = "0";
+
+        SolidColorBrush colorTitle1 = new SolidColorBrush(Color.FromArgb(100, 0, 0, 0));
+        SolidColorBrush colorText1 = new SolidColorBrush(Color.FromArgb(50, 0, 0, 0));
+
+        SolidColorBrush colorTitle2 = new SolidColorBrush(Color.FromArgb(200, 0, 0, 0));
+        SolidColorBrush colorText2 = new SolidColorBrush(Color.FromArgb(150, 0, 0, 0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Directory.CreateDirectory(localNNotesPath);
+           
+            if (File.Exists(settingsFilePath))
+            {
+                StreamReader file = new StreamReader(settingsFilePath);
+
+                this.Left = Double.Parse(file.ReadLine());
+                this.Top = Double.Parse(file.ReadLine());
+                this.Width = Double.Parse(file.ReadLine());
+                this.Height = Double.Parse(file.ReadLine());
+                darkTheme = file.ReadLine();
+
+                file.Close();
+            }
+            else
+            {
+                saveSettings();
+            }
+
+            updateTheme();
+
+            if (File.Exists(textFilePath))
+            {
+                string tmpText = File.ReadAllText(textFilePath);
+                richTextBox.Document.Blocks.Clear();
+                richTextBox.Document.Blocks.Add(new Paragraph(new Run(tmpText)));
+            }
+        }
+
+        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.ResizeMode = System.Windows.ResizeMode.CanResizeWithGrip;
+
+            imageTheme.Visibility = Visibility.Visible;
+        }
+
+        private async void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imageTheme.Visibility = Visibility.Hidden;
+
+            saveText();
+            saveSettings();
+
+            await Task.Delay(2000);
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;
+
+        }
+
+        private void label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+            }
+            catch { }
+            
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            saveText();
+        }
+
+        private void saveText()
+        {
+            string richText = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+            try
+            {
+                richText = richText.Remove(richText.Length - 2);
+            }
+            catch { }
+            File.WriteAllText(textFilePath, richText);
+        }
+
+        private void saveSettings()
+        {
+            string x = this.Left.ToString();
+            string y = this.Top.ToString();
+            string w = this.ActualWidth.ToString();
+            string h = this.ActualHeight.ToString();
+
+            string[] lines = { x, y, w, h, darkTheme };
+            File.WriteAllLines(settingsFilePath, lines);
+        }
+
+        private void updateTheme()
+        {
+            if (darkTheme == "0")
+            {
+                label.Background = colorTitle1;
+                richTextBox.Background = colorText1;
+            }
+            else
+            {
+                label.Background = colorTitle2;
+                richTextBox.Background = colorText2;
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            gridBtn.Width = this.Width;
+        }
+
+        private void imageClose_MouseEnter(object sender, MouseEventArgs e)
+        {
+            imageClose.Source = new BitmapImage(new Uri("/Resources/CloseWhiteAlpha90.png", UriKind.Relative));
+        }
+
+        private void imageClose_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imageClose.Source = new BitmapImage(new Uri("/Resources/CloseWhiteAlpha50.png", UriKind.Relative));
+        }
+
+        private void imageClose_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            imageTheme.Source = new BitmapImage(new Uri("/Resources/ThemeWhiteAlpha90.png", UriKind.Relative));
+        }
+
+        private void image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imageTheme.Source = new BitmapImage(new Uri("/Resources/ThemeWhiteAlpha50.png", UriKind.Relative));
+        }
+
+        private void imageTheme_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (darkTheme == "0")
+            {
+                darkTheme = "1";
+            }
+            else
+            {
+                darkTheme = "0";
+            }
+
+            updateTheme();
+            saveSettings();
+        }
+
+        
+    }
+}
---
+++ NNotesRestart/NNotesRestart/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NNotesRestart
+{
+    public partial class Form1 : Form
+    {
+        static string localPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        static string localNNotesPath = localPath + "\\NNotes";
+        string textFilePath = localNNotesPath + "\\Text.txt";
+        static string exePath = Application.StartupPath;
+        string exeFilePath = exePath + "\\NNotes.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(textFilePath))
+            {
+                string text = File.ReadAllText(textFilePath);
+                if(text != "")
+                {
+                    try
+                    {
+                        Process nnotes = new Process();
+                        nnotes.StartInfo.FileName = exeFilePath;
+                        nnotes.Start();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Could not start NNotes! Please put NNotesRestart.exe back in the folder with NNotes.exe!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+            }
+
+            Application.Exit();
+
+        }
+    }
+}
